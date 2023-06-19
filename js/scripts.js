91652abffb066a229601397905c69822c44b3e55
CobbyElsonfx <andohfrancis9187@gmail.com>
2023-06-19 22:27:39 +0000

    added hamburger event listener

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,26 @@ const navigationMenu = document.querySelector("#header__nav");
 const navigationLinks = document.querySelectorAll(".header__list--item");
 
 const navigationIcons = ["../images/icons/burguer.svg", "../images/icons/x-icon.svg"];
+navigationLinks.forEach((element) => {
+	element.addEventListener("click", () => {
+		toggleNavigation();
+		changeButtonIcon(navigationIcons[0]);
+		buttonMenu.style.width = "initial";
+	});
+});
+
+buttonMenu.addEventListener("click", () => {
+	const actualButtonIcon = buttonImage.src;
+
+	if (actualButtonIcon.includes("burguer")) {
+		changeButtonIcon(navigationIcons[1]);
+		buttonMenu.style.width = "12px";
+	} else {
+		changeButtonIcon(navigationIcons[0]);
+		buttonMenu.style.width = "initial";
+	}
+	toggleNavigation();
+});
 
 function changeButtonIcon(path) {
 	buttonImage.src = path;
