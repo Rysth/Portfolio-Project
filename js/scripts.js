88f4b1acc959799fb519d13f98ab848b73a7f0f2
Rysth <johnpalacios.t@gmail.com>
2023-06-20 10:06:28 -0500

    Fixed the CSS styles and improved JavaScript file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,23 @@
-const buttonMenu = document.querySelector("#header__button");
-const buttonImage = document.querySelector("#header__button--image");
+const buttonOpen = document.querySelector('#header__button--open');
+const buttonClose = document.querySelector('#header__button--close');
 
-const navigationMenu = document.querySelector("#header__nav");
-const navigationLinks = document.querySelectorAll(".header__list--item");
-
-const navigationIcons = ["../images/icons/burguer.svg", "../images/icons/x-icon.svg"];
-navigationLinks.forEach((element) => {
-	element.addEventListener("click", () => {
-		toggleNavigation();
-		changeButtonIcon(navigationIcons[0]);
-		buttonMenu.style.width = "initial";
-	});
-});
-
-buttonMenu.addEventListener("click", () => {
-	const actualButtonIcon = buttonImage.src;
-
-	if (actualButtonIcon.includes("burguer")) {
-		changeButtonIcon(navigationIcons[1]);
-		buttonMenu.style.width = "12px";
-	} else {
-		changeButtonIcon(navigationIcons[0]);
-		buttonMenu.style.width = "initial";
-	}
-	toggleNavigation();
-});
-
-function changeButtonIcon(path) {
-	buttonImage.src = path;
-}
+const navigationMenu = document.querySelector('#header__nav');
+const navigationLinks = document.querySelectorAll('.header__list--item');
 
 function toggleNavigation() {
-	navigationMenu.classList.toggle("header__nav--active");
+  navigationMenu.classList.toggle('header__nav--active');
 }
+
+navigationLinks.forEach((element) => {
+  element.addEventListener('click', () => {
+    toggleNavigation();
+  });
+});
+
+buttonOpen.addEventListener('click', () => {
+  toggleNavigation();
+});
+
+buttonClose.addEventListener('click', () => {
+  toggleNavigation();
+});
